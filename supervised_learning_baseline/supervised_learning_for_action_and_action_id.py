f970ef6c9ef53552a4ba6e1666969e62126d6645
Xuan Tong <txmanialife@gmail.com>
2018-04-30 14:15:39 +1000

    add lstm to action_id

--- supervised_learning_baseline/supervised_learning_for_action_and_action_id.py
+++ supervised_learning_baseline/supervised_learning_for_action_and_action_id.py
@@ -75,13 +75,15 @@ KEEP_PROB = 0.7
 LAYER_NUM = 2
 input_to_rnn = tf.reshape(input_to_classification, [-1, 24, 24])
 lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=HIDDEN_SIZE, forget_bias=1.0, state_is_tuple=True)
-lstm_cell = tf.contrib.rnn.DropoutWrapper(cell=lstm_cell, input_keep_prob=1.0, output_keep_prob=KEEP_PROB)
-mlstm_cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * LAYER_NUM, state_is_tuple=True)
-batch_size = tf.shape(input_to_rnn)[0]
-init_state = mlstm_cell.zero_state(batch_size, dtype=tf.float32)
+inputs_rnn = tf.unstack(input_to_rnn, num=24, axis=1)
+rnn_outputs,rnn_state=rnn.static_rnn(lstm_cell,inputs_rnn,dtype=tf.float32)
+# lstm_cell = tf.contrib.rnn.DropoutWrapper(cell=lstm_cell, input_keep_prob=1.0, output_keep_prob=KEEP_PROB)
+# mlstm_cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * LAYER_NUM, state_is_tuple=True)
+# batch_size = tf.shape(input_to_rnn)[0]
+# init_state = mlstm_cell.zero_state(batch_size, dtype=tf.float32)
 # rnn_outputs, rnn_state = tf.nn.dynamic_rnn(mlstm_cell, inputs=input_to_rnn, initial_state=init_state, time_major=False)
-inputs_rnn = tf.unstack(input_to_rnn, num=self.num_steps, axis=1)
-rnn_outputs, rnn_state = tf.nn.static_rnn(mlstm_cell, inputs_rnn, initial_state=init_state)
+# inputs_rnn = tf.unstack(input_to_rnn, num=self.num_steps, axis=1)
+# rnn_outputs, rnn_state = tf.nn.static_rnn(mlstm_cell, inputs_rnn, initial_state=init_state)
 input_to_classification = rnn_state[-1][1] # shape is [batch_size, HIDDEN_SIZE]
 
 # lstm_layer = tf.nn.rnn_cell.BasicLSTMCell(RNN_HIDDEN, forget_bias=1, state_is_tuple=True)
